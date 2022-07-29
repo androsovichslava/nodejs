800dc0c47c744f6f7bd4edf484f557ef39b4092d
slava <slava@ukr.net>
2022-07-29 23:21:28 +0300

    edited

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import {cube, square} from './math.js'
 import { open, read, close } from 'fs';
+import _ from 'underscore';
 
 
 console.log(cube(4) + square(7));
 
 
+console.log(_.last(['a', 'b', 'c', 'd'], 2));
